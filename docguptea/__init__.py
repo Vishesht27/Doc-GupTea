cbb2f5f409a15a447444fc284c31b7ec025e24a8
Mayuresh Agashe <mayureshagashe2002@gmail.com>
2023-07-01 14:29:45 +0530

    initial commit

---
+++ docguptea/__init__.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from docguptea import router
+
+app = FastAPI(title="DocGup-Tea",
+              version="V0.0.1",
+              description="API for automatic documentation generation!"
+              )
